4c5b72785b68283d80f7f8697a49f49e2a439d76
Moaiz Abrar <93871723+moaiz237@users.noreply.github.com>
2023-10-02 10:05:26 +0500

    Update download.py

--- download.py
+++ download.py
@@ -3,7 +3,8 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import WhisperForConditionalGeneration, WhisperConfig, AutoProcessor
+from transformers import WhisperForConditionalGeneration, WhisperConfig, AutoProcessor, WhisperProcessor, WhisperTokenizer
+from peft import PeftModel, PeftConfig
 from utils import custom_save
 
 
@@ -12,9 +13,18 @@ def download_model():
     
     # processor = AutoProcessor.from_pretrained("openai/whisper-large-v2")
     # config = WhisperConfig.from_pretrained("openai/whisper-base")
-    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-large-v2", language="Urdu", task="transcribe")
-    processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2", language="Urdu", task="transcribe")
-    model = WhisperForConditionalGeneration.from_pretrained("Moaiz/whisper-fine-tune-LoRA-roman-urdu-add-2")
+    # tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-large-v2", language="Urdu", task="transcribe")
+    # processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2", language="Urdu", task="transcribe")
+    # model = WhisperForConditionalGeneration.from_pretrained("Moaiz/whisper-fine-tune-LoRA-roman-urdu-add-2")
+
+    peft_model_id = "Moaiz/whisper-fine-tune-LoRA-roman-urdu-add-2" # Use the same model ID as before.
+    language = "Urdu"
+    task = "transcribe"
+    peft_config = PeftConfig.from_pretrained(peft_model_id)
+    model = WhisperForConditionalGeneration.from_pretrained(peft_config.base_model_name_or_path)
+    model = PeftModel.from_pretrained(model, peft_model_id, token = "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")
+    tokenizer = WhisperTokenizer.from_pretrained(peft_config.base_model_name_or_path, language=language, task=task)
+    processor = WhisperProcessor.from_pretrained(peft_config.base_model_name_or_path, language=language, task=task)
     custom_save(model)
 
 if __name__ == "__main__":
